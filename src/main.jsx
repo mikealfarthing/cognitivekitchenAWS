1c1d720092e7c65f5a11625b880bdc0c25210804
Mike Farthing <mfarthing@ipfusion.ca>
2026-08-08 18:46:54 -0600

    cognitive kitchen theming

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,30 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { Authenticator } from "@aws-amplify/ui-react";
+import { Authenticator, ThemeProvider, createTheme } from "@aws-amplify/ui-react";
+
+const cognitiveKitchenTheme = createTheme({
+  name: "cognitive-kitchen",
+  tokens: {
+    colors: {
+      brand: {
+        primary: {
+          10: "#eef0f5",
+          80: "#3c405b",
+          90: "#2c2f45", 
+          100: "#2c2f45",
+        },
+      },
+    },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
-<Authenticator>
-<App />
-</Authenticator>
-</React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={cognitiveKitchenTheme}>
+      <Authenticator>
+        <App />
+      </Authenticator>
+    </ThemeProvider>
+  </React.StrictMode>
 );
